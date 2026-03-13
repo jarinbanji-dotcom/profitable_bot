dcbde7fc12716db3bf61e819ba870650498282e6
jarinbanji-dotcom <jarinbanji@gmail.com>
2026-03-13 16:29:30 +0530

    Update bybit.py

--- bybit.py
+++ bybit.py
@@ -38,6 +38,9 @@ def run_session_continously(symbol):
     t2.start()
     t1.join()
     t2.join()
+    print(f"Both fetched in: {time.time()-st:.3f}s")
+
+
 
 
 def place_tp_order(order_id, tp_percent, symbol):
@@ -322,6 +325,7 @@ print("step size : ", price_tick)
 print("qty step", qty_step)
 print("tp price : ", tp_price_raw)
 """
+
 
 
 
